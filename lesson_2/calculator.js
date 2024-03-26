9f91147092ee8aef2f4d140c6e93eb785b6dc657
Gautam Panakkal <gautampanakkal@gmail.com>
2024-03-25 19:06:31 -0700

    added repeat functionality to calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -3,25 +3,47 @@
  */
 const rl = require('readline-sync');
 
-console.log('Welcome to Calculator!');
-const x = parseFloat(rl.question("first number: "));
-const y = parseFloat(rl.question("second number: "));
-const operation = parseInt(rl.question("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide: "));
+const formattedPrompt = (message) => console.log(`=> ${message}`);
+const validNumber = (numberStr) => !(numberStr.trimStart() === '' || Number.isNaN(Number(numberStr)));
 
-if (Number.isNaN(x) || Number.isNaN(y)) {
-  console.log(`one of the numbers is invalid`);
-  return;
-}
+const getValidNumberInput = (prompt) => {
+  formattedPrompt(prompt);
+  let input = rl.question().replace(/,/, '');
+  while (!validNumber(input)) {
+    console.log('Not a valid number; try again');
+    input = rl.question().replace(/,/, '');
+  }
+  return Number(input);
+};
 
 const operationDict = {
   1: (x, y) => x + y,
   2: (x, y) => x - y,
   3: (x, y) => x * y,
   4: (x, y) => x / y,
+};
+
+console.log('Welcome to Calculator!');
+
+function main() {
+  const x = getValidNumberInput('First number:');
+  const y = getValidNumberInput('Second number:');
+
+  formattedPrompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide:');
+  let operation = rl.question();
+  while (operationDict[operation] === undefined) {
+    console.log('Not a valid option; try again');
+    operation = rl.question();
+  }
+
+  console.log(operationDict[operation](x, y));
+  formattedPrompt('Perform another calculation? y / n');
+  let repeat = rl.question();
+  while (repeat !== 'y' && repeat !== 'n') {
+    console.log('Not a valid option; type y or n');
+    repeat = rl.question();
+  }
+  if (repeat === 'y') main();
 }
 
-if (!(operation in operationDict)) {
-  console.log('invalid operator');
-  return;
-}
-console.log(operationDict[operation](x, y));
+main();
